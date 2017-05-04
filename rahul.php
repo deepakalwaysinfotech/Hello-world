16b7ba120b469ea492c74a39447c483bda1dde89
deepakalwaysinfotech <developer1@alwaysinfotech.com>
2017-05-04 17:10:03 +0530

    testing

--- rahul.php
+++ rahul.php
@@ -4,12 +4,9 @@
 </head>
 <body>
 	<h1>Welcome</h1>
-	<h2>Hello World</h2>
-	<h2>Hello World</h2>
-	<h2>Hello World</h2>
-	<h2>Hello World</h2>
-	<h2>Hello World</h2>
-	<h2>Hello World</h2>
+	<h1>Welcome</h1>
+	<h1>Welcome</h1>
+	
 
 </body>
 </html>
